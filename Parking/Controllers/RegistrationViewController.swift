11859eb473d245a793407137a206e0deaf99bb41
margulan9 <sugirbaymargulan@gmail.com>
2020-02-04 16:11:59 +0600

    implement sign in/sign up ui

---
+++ Parking/Controllers/RegistrationViewController.swift
@@ -0,0 +1,46 @@
+//
+//  RegistrationViewController.swift
+//  Parking
+//
+//  Created by Sugirbay Margulan on 2/4/20.
+//  Copyright © 2020 Sugirbay Margulan. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailOrPhoneTF: UITextField!
+    @IBOutlet weak var vehicleNumberTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        emailOrPhoneTF.addLine(position: .LINE_POSITION_BOTTOM, color: #colorLiteral(red: 0.6666666667, green: 0.7137254902, blue: 0.7647058824, alpha: 1), width: 0.5)
+        vehicleNumberTF.addLine(position: .LINE_POSITION_BOTTOM, color: #colorLiteral(red: 0.6666666667, green: 0.7137254902, blue: 0.7647058824, alpha: 1), width: 0.5)
+        passwordTF.addLine(position: .LINE_POSITION_BOTTOM, color: #colorLiteral(red: 0.6666666667, green: 0.7137254902, blue: 0.7647058824, alpha: 1), width: 0.5)
+    }
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension RegistrationViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        textField.addLine(position: .LINE_POSITION_BOTTOM, color: #colorLiteral(red: 0.05098039216, green: 0.7137254902, blue: 0.3960784314, alpha: 1), width: 0.5)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        textField.addLine(position: .LINE_POSITION_BOTTOM, color: #colorLiteral(red: 0.6666666667, green: 0.7137254902, blue: 0.7647058824, alpha: 1), width: 0.5)
+    }
+}
---
+++ Parking/Helpers/Constants.swift
@@ -0,0 +1,39 @@
+//
+//  Constants.swift
+//  Parking
+//
+//  Created by Sugirbay Margulan on 2/4/20.
+//  Copyright © 2020 Sugirbay Margulan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+// line for textfieds
+enum LINE_POSITION {
+    case LINE_POSITION_TOP
+    case LINE_POSITION_BOTTOM
+}
+
+extension UIView {
+    func addLine(position : LINE_POSITION, color: UIColor, width: Double) {
+        let lineView = UIView()
+        lineView.backgroundColor = color
+        lineView.translatesAutoresizingMaskIntoConstraints = false // This is important!
+        self.addSubview(lineView)
+
+        let metrics = ["width" : NSNumber(value: width)]
+        let views = ["lineView" : lineView]
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[lineView]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+
+        switch position {
+        case .LINE_POSITION_TOP:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[lineView(width)]", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        case .LINE_POSITION_BOTTOM:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[lineView(width)]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        }
+    }
+}
